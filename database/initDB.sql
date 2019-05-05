bf7b7c0ccebbfac19120d251761faae8ed98ddb4
bitfishxyz@gmail.com <bitfishxyz@gmail.com>
2019-05-05 20:33:22 +0800

    优化注释，删除无用的文件

--- database/initDB.sql
+++ database/initDB.sql
@@ -1,6 +1,15 @@
 create database if not exists mybatis_generator;
-
 use mybatis_generator;
-show tables ;
 
-desc monkey;
+drop table if exists monkey;
+create table monkey(
+  id int primary key auto_increment,
+  name char(20) not null,
+  phone_number int,
+  birthday date
+);
+
+insert into monkey(name, phone_number, birthday)
+VALUES ('Jon Snow', 4794062, '1989-06-04');
+
+select * from monkey limit 30;
